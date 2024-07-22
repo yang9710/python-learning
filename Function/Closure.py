f77cc6208425a5cac8728e737b471306b74aec41
刘 成阳 <chengyang.liu@univers.com>
2024-07-22 22:31:14 +0800

    add .

---
+++ Function/Closure.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+
+def urltemplate(template):
+    def opener(**kwargs):
+        return urlopen(template.format_map(kwargs))
+    return opener
+
+# Example use
+yahoo = urltemplate('http://finance.yahoo.com/d/quotes.csv?s={names}&f={fields}')
+for line in yahoo(names='IBM,AAPL,FB', fields='sl1c1v'):
+  print(line.decode('utf-8'))
---
+++ Function/MultiVariable.py
@@ -0,0 +1,22 @@
+import html
+
+def make_element(name, value, **attrs):
+    keyvals = [' %s="%s"' % item for item in attrs.items()]
+    attr_str = ''.join(keyvals)
+    element = '<{name}{attrs}>{value}</{name}>'.format(
+                name=name,
+                attrs=attr_str,
+                value=html.escape(value))
+    return element
+
+# Example
+# Creates '<item size="large" quantity="6">Albatross</item>'
+make_element('item', 'Albatross', size='large', quantity=6)
+
+# Creates '<p>&lt;spam&gt;</p>'
+make_element('p', '<spam>')
+
+def foo(a, *args, c,**kwds):
+    print(a, args,c, kwds)
+
+foo('foo','1',111111, b=1,r=2)
